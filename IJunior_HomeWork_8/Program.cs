34e75c2bde67f5045db771b2800b45cff387a38e
Demon99999 <35568272+Demon99999@users.noreply.github.com>
2022-07-01 18:09:57 +0300

    First commit

---
+++ IJunior_HomeWork_8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IJunior_HomeWork_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countPeople;
+            int receptionTimeOfOnePerson = 10;
+            Console.WriteLine("Сколько человек в очереди ?");
+            countPeople = Convert.ToInt32(Console.ReadLine());
+            int totalTime = countPeople * receptionTimeOfOnePerson;
+            int minutesInHour = 60;
+            int countHours = totalTime/ minutesInHour;
+            int countMinutes = totalTime % minutesInHour;
+            Console.WriteLine($"Вы должны стоять в очереди {countHours} часа и {countMinutes} минут.");
+        }
+    }
+}
